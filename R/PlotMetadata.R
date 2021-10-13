c758e267ffa0fb267160fb8af1c87fef4c25fb80
Laurence-Traynor <alaurencetraynor@blm.gov>
2021-10-13 17:36:43 -0600

    Update PlotMetadata.R

--- R/PlotMetadata.R
+++ R/PlotMetadata.R
@@ -4,13 +4,11 @@ PlotMetadata <- function(dataframe, EcologicalSite){
   #Create color palettes 
   Plots_Simple <- dataframe %>% 
     filter(EcologicalSiteId == EcologicalSite)%>%
-          dplyr::select(PrimaryKey, Time_Period) %>%   
-             dplyr::mutate(PlotsPerYear = Plots) %>%
-                dplyr::arrange(Time_Period)
+          dplyr::select(PrimaryKey) %>%   
+             dplyr::mutate(PlotsPerYear = Plots)
 
   PlotsPerYear <- ggplot2::ggplot(Plots_Simple , aes(x= Plots, text = stat(count))) +
-    geom_bar(position = position_stack(reverse = TRUE) ,
-             aes(fill = Time_Period) , width = .2) +
+    geom_bar(position = position_stack(reverse = TRUE), width = .2) +
     scale_fill_brewer(palette = 7, type = "div") +
     ggtitle(paste("Plots Per Time Period in", EcologicalSite)) +
     theme_minimal()+
