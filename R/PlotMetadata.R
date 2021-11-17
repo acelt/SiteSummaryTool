7feccf75f851b3bcdd43d38460ae35804fe968d2
Laurence-Traynor <alaurencetraynor@blm.gov>
2021-11-17 10:02:06 -0700

    Update PlotMetadata.R

--- R/PlotMetadata.R
+++ R/PlotMetadata.R
@@ -1,15 +1,16 @@
-PlotMetadata <- function(dataframe){
+PlotMetadata <- function(dataframe, EcologicalSite){
   Plots <- c("Plots")
 
   #Create color palettes 
-  Plots_Simple <- dataframe %>% 
+  Plots_Simple <- dataframe[dataframe$EcologicalSiteId == EcologicalSite,] %>% 
           dplyr::select(PrimaryKey, EcologicalSiteId) %>%   
              dplyr::mutate(PlotsPerYear = Plots)
 
   PlotsPerYear <- ggplot2::ggplot(Plots_Simple , aes(x= Plots, text = stat(count), fill = EcologicalSiteId)) +
     geom_bar(position = "dodge") +
-    ggtitle("Plots Per Ecoregion") +
+    ggtitle("Count of Plots") +
     theme_minimal()+
+    theme(legend.title = element_blank())+
     coord_flip() +
     theme(axis.text.y = element_blank(), legend.position = "bottom")
   
